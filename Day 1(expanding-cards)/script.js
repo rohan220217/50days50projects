2a67d43c3ea8867fc3537a9fdd5fd921ac47a248
Rohan kumar <rk220217@gmail.com>
2023-01-29 12:02:15 +0530

    progress project added

--- Day 1(expanding-cards)/script.js
+++ Day 1(expanding-cards)/script.js
@@ -1,14 +1,50 @@
-const panels = document.querySelectorAll('.panel')
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+let currentIndex = 1;
 
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+next.addEventListener("click", () => {
+  currentIndex++;
+
+  if (currentIndex > circles.length) {
+    currentIndex = circles.length;
+  }
+
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentIndex--;
+
+  if (currentIndex < 1) {
+    currentIndex = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, circleInd) => {
+    if (circleInd < currentIndex) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const activeCircles = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((activeCircles.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentIndex === 1) {
+    prev.disabled = true;
+  } else if (currentIndex === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 }
